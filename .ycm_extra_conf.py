6cffe77e9ef527d79976c0be6b42feda5738b05c
theskyinlake <theskyinlake@qq.com>
2019-03-13 22:24:56 +0800

    201903132225

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,18 @@
+flags = [
+        '-Wall',
+        '-Wextra',
+        '-Werror',
+        '-fexceptions',
+        '-DNDEBUG', 
+        '-std=c++11', 
+        '-x',
+        'c++',
+        '-isystem',
+        '/usr/include',
+        '-isystem',
+        '/usr/include/x86_64-linux-gnu/c++/',
+        '-isystem',
+        '/usr/include/c++/5.4.0',
+        '-isystem',
+        './header',
+        ]
